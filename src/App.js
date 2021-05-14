24790185fc16b52e45dca247f8c795d8b2d2ddd9
Andrew Parsons <77991510+tvnames@users.noreply.github.com>
2021-05-14 14:53:43 -0700

    Added search input

--- src/App.js
+++ src/App.js
@@ -1,8 +1,90 @@
 function App() {
   return (
     <div>
-      <h1>React Employee Directory</h1>
-      <p> Nothing yet! Please come back soon to see!</p>
+      <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+          <h1 class="display-4">React Employee Directory</h1>
+          <p class="lead">Employee List</p>
+        </div>
+
+        <form class="form-inline">
+          <label class="sr-only" for="inlineFormInputName2">
+            Name
+          </label>
+          <input
+            type="text"
+            class="form-control mb-2 mr-sm-2"
+            id="inlineFormInputName2"
+            placeholder="Jane Doe"
+          />
+
+          <button type="submit" class="btn btn-primary mb-2">
+            Search
+          </button>
+        </form>
+
+        <table class="table">
+          <thead class="thead-dark">
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">First</th>
+              <th scope="col">Last</th>
+              <th scope="col">Handle</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td>Larry</td>
+              <td>the Bird</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </table>
+
+        <table class="table">
+          <thead class="thead-light">
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">First</th>
+              <th scope="col">Last</th>
+              <th scope="col">Handle</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td>Larry</td>
+              <td>the Bird</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
